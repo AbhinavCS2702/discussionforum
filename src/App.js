0258a3616b4ff5df1fd8ce2180c299e5c1cb18e5
rakesh <rakeshhotker@gmail.com>
2021-12-26 23:21:22 +0530

    safety bitch

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 
 import './App.css';
 import Login from './Login.js';
-
+import React from 'react';
 function App() {
   return (
     <div className="App">
---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBw6el7LEnStZDcIWgMuvIV7CfGvCoHlTw",
+  authDomain: "discussionforum-706f6.firebaseapp.com",
+  projectId: "discussionforum-706f6",
+  storageBucket: "discussionforum-706f6.appspot.com",
+  messagingSenderId: "527080493516",
+  appId: "1:527080493516:web:1eed207cd9e69982974d69"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
